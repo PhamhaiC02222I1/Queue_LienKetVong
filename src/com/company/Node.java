7d66514b22c418a5fea6de3b64239d0c4d965de9
hai <humg.ola@gmail.com>
2022-04-04 16:00:35 +0700

    up

---
+++ src/com/company/Node.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Node {
+    public int key;
+    public Node link;
+
+    public Node(int key, Node link) {
+        this.key = key;
+        this.link = link;
+    }
+
+}
